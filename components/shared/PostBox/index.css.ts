31179cccebe5772a86fa3863ee9b45e7c0247a9e
arfes0e2b3c <webyata@gmail.com>
2023-10-21 11:48:43 +0900

    feat: PostBoxの実装

---
+++ components/shared/PostBox/index.css.ts
@@ -0,0 +1,86 @@
+import { style } from '@vanilla-extract/css'
+
+export const postBox = style({
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  marginTop: '20px',
+  marginBottom: '10px',
+  overflow: 'hidden',
+  transition: '0.5s',
+  '@media': {
+    'screen and (max-width: 768px)': {
+      marginTop: '10px',
+    },
+  },
+})
+
+export const postBoxTitle = style({
+  margin: '0px auto',
+  fontSize: '1.4rem',
+  '@media': {
+    'screen and (max-width: 768px)': {
+      fontSize: '1.2rem',
+    },
+  },
+})
+
+export const postBoxInput = style({
+  resize: 'none',
+  width: '100%',
+  height: '80px',
+  padding: '20px',
+  outline: 'none',
+  border: 'none',
+  borderBottom: '2px solid #333',
+  borderColor: 'rgba(0, 0, 0, 0.15)',
+  borderWidth: '2px',
+  borderRadius: '0',
+  fontSize: '1rem',
+  transition: '.2s',
+  ':focus': {
+    borderColor: 'rgba(0, 0, 0, 0.5)',
+  },
+  '@media': {
+    'screen and (max-width: 768px)': {
+      height: '60px',
+      padding: '10px 20px',
+      fontSize: '0.8rem',
+    },
+  },
+})
+
+export const noticeMessage = style({
+  margin: '0',
+  marginTop: '10px',
+  fontSize: '1rem',
+  '@media': {
+    'screen and (max-width: 768px)': {
+      fontSize: '0.8rem',
+    },
+  },
+})
+
+export const submitButton = style({
+  width: '120px',
+  height: '40px',
+  marginTop: '20px',
+  backgroundColor: '#333',
+  color: 'white',
+  border: 'none',
+  borderRadius: '3px',
+  transition: '.1s',
+  cursor: 'pointer',
+  ':hover': {
+    backgroundColor: 'white',
+    color: '#333',
+    boxShadow: '0 0 5px 5px rgba(0, 0, 0, 0.1)',
+  },
+  '@media': {
+    'screen and (max-width: 768px)': {
+      height: '35px',
+      marginTop: '10px',
+      fontSize: '0.8rem',
+    },
+  },
+})
---
+++ components/shared/PostBox/index.tsx
@@ -0,0 +1,38 @@
+'use client'
+
+import { baseFont } from '@/consts/fonts'
+import { noticeMessage, postBox, postBoxInput, postBoxTitle, submitButton } from './index.css'
+
+export const PostForm = ({ mode }: { mode: string }) => {
+  const isQuestion = mode === 'question'
+  const formProps = isQuestion
+    ? {
+        buttonText: '質問する',
+        onSubmit: () => console.log('question'),
+        placeholder: '例：１年経済学部です。履修登録はどこでできますか？',
+      }
+    : {
+        buttonText: '情報を提供する',
+        onSubmit: () => console.log('reply'),
+        placeholder: 'この質問に関する情報提供をいただけますと幸いです！',
+      }
+  return (
+    <form className={postBox}>
+      {isQuestion && <h3 className={postBoxTitle}>お手伝いサークルに質問する</h3>}
+      <textarea
+        className={[postBoxInput, baseFont.className].join(' ')}
+        name='question'
+        id=''
+        cols={30}
+        rows={10}
+        placeholder={formProps.placeholder}
+      ></textarea>
+      {isQuestion && (
+        <p className={noticeMessage}>正確な回答のため、学部と学年の併記をお願いします！</p>
+      )}
+      <button className={[submitButton, baseFont.className].join(' ')} onClick={formProps.onSubmit}>
+        {formProps.buttonText}
+      </button>
+    </form>
+  )
+}
