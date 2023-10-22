ea8062e2062f8fcc435daa0ecbdda9dd9805c53b
arfes0e2b3c <webyata@gmail.com>
2023-10-22 13:30:28 +0900

    feat: 個別ページの実装

---
+++ app/[questionId]/page.tsx
@@ -0,0 +1,28 @@
+import { PostForm } from '@/components/shared/PostBox'
+import { MicroCMSResponse } from '@/types'
+import { QACardContainer } from '@/components/shared/QACardContainer'
+import { formContainer, individualPage, qAContainer } from './page.css'
+
+export default async function IndividualPage({ params }: { params: { questionId: string } }) {
+  let res: MicroCMSResponse = { contents: [], totalCount: 0, offset: 0, limit: 0 }
+  await fetch(
+    'https://q-box.microcms.io/api/v1/q_box_posts?filters=id[equals]' + params.questionId,
+    {
+      headers: { 'X-MICROCMS-API-KEY': process.env.NEXT_PUBLIC_MICROCMS_KEY || '' },
+      next: { revalidate: 3600 },
+    }
+  )
+    .then((response) => response.json())
+    .then((data) => (res = data))
+  return (
+    <main className={individualPage}>
+      <div className={qAContainer}>
+        <QACardContainer qAData={res.contents[0]} />
+        <PostForm mode={'reply'} />
+      </div>
+      <div className={formContainer}>
+        <PostForm mode={'question'} />
+      </div>
+    </main>
+  )
+}
