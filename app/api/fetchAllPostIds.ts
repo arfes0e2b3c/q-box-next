34c5243f97a832a5318b7f2b4d2323ceea9179da
arfes0e2b3c <webyata@gmail.com>
2023-10-25 04:12:44 +0900

    fix: URL指定の修正

--- app/api/fetchAllPostIds.ts
+++ app/api/fetchAllPostIds.ts
@@ -1,7 +1,7 @@
 import { qA } from '@/types'
 
 export const fetchAllPostIds = async () => {
-  const res = await fetch(`${process.env.VERCEL_URL}/api/all_ids`)
+  const res = await fetch(`https://${process.env.VERCEL_URL}/api/all_ids`)
   const { data }: { data: qA[] } = await res.json()
   return data
 }
--- app/api/fetchSlicePosts.ts
+++ app/api/fetchSlicePosts.ts
@@ -1,7 +1,7 @@
 import { MicroCMSResponse } from '@/types'
 
 export const fetchSlicePosts = async (offset: number) => {
-  const res = await fetch(`${process.env.VERCEL_URL}/api/posts?offset=${offset}`)
+  const res = await fetch(`https://${process.env.VERCEL_URL}/api/posts?offset=${offset}`)
   const data: MicroCMSResponse = await res.json()
   return data
 }
