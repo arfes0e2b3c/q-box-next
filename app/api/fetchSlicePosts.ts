5a7edf2268268c57690e9ab7e7bf1eb7cdb147e9
arfes0e2b3c <webyata@gmail.com>
2023-10-25 02:42:27 +0900

    refactor: originの切り出し

--- app/api/fetchSlicePosts.ts
+++ app/api/fetchSlicePosts.ts
@@ -1,7 +1,7 @@
 import { MicroCMSResponse } from '@/types'
 
 export const fetchSlicePosts = async (offset: number) => {
-  const res = await fetch(`http://localhost:3000/api/posts?offset=${offset}`)
+  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/posts?offset=${offset}`)
   const data: MicroCMSResponse = await res.json()
   return data
 }
