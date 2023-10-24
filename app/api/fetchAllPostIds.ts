4d3fea207104404e224d2164ab4a220b7960b12a
arfes0e2b3c <webyata@gmail.com>
2023-10-25 04:10:54 +0900

    fix: 環境変数の変更

--- app/api/fetchAllPostIds.ts
+++ app/api/fetchAllPostIds.ts
@@ -1,7 +1,7 @@
 import { qA } from '@/types'
 
 export const fetchAllPostIds = async () => {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/all_ids`)
+  const res = await fetch(`${process.env.VERCEL_URL}/api/all_ids`)
   const { data }: { data: qA[] } = await res.json()
   return data
 }
