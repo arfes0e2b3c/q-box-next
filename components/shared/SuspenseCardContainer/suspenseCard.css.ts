2ba9235f992c7f33f51d3000013be9a66d3153d7
arfes0e2b3c <webyata@gmail.com>
2023-10-26 20:40:47 +0900

    feat: Suspenseの実装

---
+++ components/shared/SuspenseCardContainer/suspenseCard.css.ts
@@ -0,0 +1,25 @@
+import { bgAnimation } from '@/consts'
+import { style } from '@vanilla-extract/css'
+
+export const suspenseCard = style({
+  position: 'relative',
+  width: '100%',
+  height: 'auto',
+  aspectRatio: '40 / 21',
+  background: '#ddd',
+  marginTop: '10px',
+  borderRadius: '15px',
+  overflow: 'hidden',
+  ':after': {
+    content: '""',
+    position: 'absolute',
+    top: '0',
+    left: '0',
+    width: '100%',
+    height: '100%',
+    background:
+      'linear-gradient(135deg, transparent,20%, transparent, 40%, #e3e3e3,60%,transparent, 80%, transparent)',
+    backgroundRepeat: 'no-repeat',
+    animation: `${bgAnimation} 2s ease infinite`,
+  },
+})
--- consts/index.ts
+++ consts/index.ts
@@ -1,4 +1,5 @@
 import { MicroCMSResponse } from '@/types'
+import { keyframes } from '@vanilla-extract/css'
 
 export const baseTransition = '0.3s'
 export const shortTransition = '0.1s'
@@ -8,6 +9,12 @@ export const spHeaderHeight = '60px'
 
 export const pcFooterHeight = '150px'
 export const spFooterHeight = '100px'
+
+export const bgAnimation = keyframes({
+  '0%': { left: '-100%' },
+  '50%': { left: '100%' },
+  '100%': { left: '100%' },
+})
 
 export const mockQAResponse: MicroCMSResponse = {
   contents: [
