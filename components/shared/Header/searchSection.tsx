9999322a56e51c9c27e88b1c51542d7d94f80e81
arfes0e2b3c <webyata@gmail.com>
2023-10-27 14:15:18 +0900

    refactor: enterKeyイベントの修正

--- components/shared/Header/searchSection.tsx
+++ components/shared/Header/searchSection.tsx
@@ -9,15 +9,17 @@ import { useRouter } from 'next/navigation'
 export const SearchSection = () => {
   const router = useRouter()
   const [isOpen, setIsOpen] = useState(false)
-  const isEnterKey = (event: React.KeyboardEvent<HTMLInputElement>) => event.key === 'Enter'
+  const isEnterKey = (key: string) => key === 'Enter'
   return (
     <section className={searchSection} onFocus={() => setIsOpen(true)}>
       <input
         className={[searchInput, baseFont.className].join(' ')}
         type='text'
-        onKeyUp={(event) =>
-          isEnterKey(event) && router.push(`/search/?q=${event.currentTarget.value}`)
-        }
+        onKeyUp={(event) => {
+          if (isEnterKey(event.key) && event.currentTarget.value !== '') {
+            router.push(`/search/?q=${event.currentTarget.value}`)
+          }
+        }}
       />
       {isOpen && <SearchModal onBlur={() => setIsOpen(false)} />}
     </section>
