cd6adf64dcc60bb770c764533c599e765cbc2f80
arfes0e2b3c <webyata@gmail.com>
2023-10-27 15:55:45 +0900

    fix: 検索の際に非公開の返信を表示しないよう修正

--- app/api/search/route.ts
+++ app/api/search/route.ts
@@ -1,6 +1,7 @@
 import { MicroCMSResponse, qA } from '@/types'
 import { NextRequest, NextResponse } from 'next/server'
 import { fetchPosts } from '../microcms'
+import { filterPublicReplies } from '@/lib'
 
 export async function GET(req: NextRequest): Promise<NextResponse<MicroCMSResponse>> {
   const { searchParams } = new URL(req.url)
@@ -10,12 +11,14 @@ export async function GET(req: NextRequest): Promise<NextResponse<MicroCMSRespon
   const limitCount = 20
   const res: MicroCMSResponse = await fetchPosts({
     queries: {
-      filters: 'answer[exists][and]replyTweetId[exists]',
+      filters: 'answer[exists]',
       limit: limitCount,
       offset: offset ? Number(offset) : 0,
       q: q,
     },
   })
+
+  res.contents = res.contents.map((post) => filterPublicReplies(post))
 
   return NextResponse.json(res)
 }
