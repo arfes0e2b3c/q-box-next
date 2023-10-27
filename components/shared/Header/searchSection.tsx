3cd9366d5181eaa4b456b5c8085c5aa7c3cb036e
arfes0e2b3c <webyata@gmail.com>
2023-10-27 14:16:04 +0900

    refactor: setIsOpenの移動

--- components/shared/Header/searchSection.tsx
+++ components/shared/Header/searchSection.tsx
@@ -11,11 +11,12 @@ export const SearchSection = () => {
   const [isOpen, setIsOpen] = useState(false)
   const isEnterKey = (key: string) => key === 'Enter'
   return (
-    <section className={searchSection} onFocus={() => setIsOpen(true)}>
+    <section className={searchSection}>
       <input
         className={[searchInput, baseFont.className].join(' ')}
         type='text'
         placeholder='キーワードで検索'
+        onFocus={() => setIsOpen(true)}
         onKeyUp={(event) => {
           if (isEnterKey(event.key) && event.currentTarget.value !== '') {
             router.push(`/search/?q=${event.currentTarget.value}`)
