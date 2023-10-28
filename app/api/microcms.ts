ee0be4aaf883a2eecc71f5a6bb0d160b9d9c007c
arfes0e2b3c <webyata@gmail.com>
2023-10-28 21:03:42 +0900

    refactor: 空行の追加

--- app/api/microcms.ts
+++ app/api/microcms.ts
@@ -1,5 +1,6 @@
 import { MicroCMSQueries, MicroCMSResponse, QA } from '@/types'
 import { createClient } from 'microcms-js-sdk'
+
 const client = createClient({
   serviceDomain: 'q-box',
   apiKey: process.env.NEXT_PUBLIC_MICROCMS_KEY || '',
