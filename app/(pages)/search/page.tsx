a94caf328afa8eb03018e8ed91c7b6545e3c6861
arfes0e2b3c <webyata@gmail.com>
2023-10-29 00:30:28 +0900

    refactor: page関連をディレクトリにまとめる

--- app/(pages)/search/page.tsx
+++ app/(pages)/search/page.tsx
@@ -1,15 +1,20 @@
 'use client'
 
 import { PostForm } from '@/components/shared/PostBox'
-import { formContainer, mainContainer, qAListTitle, topPage } from './page.css'
+import { formContainer, mainContainer, noResult, qAListTitle, topPage } from './page.css'
 import { useInfiniteQuery } from '@tanstack/react-query'
-import { fetchSlicePosts } from './client/fetchSlicePosts'
 import { QAListWrapper } from '@/components/shared/qAListWrapper'
+import { useSearchParams } from 'next/navigation'
+import { fetchSliceSearchPosts } from '../../client/fetchSliceSearchPosts'
+import { QACardContainer } from '@/components/shared/QACardContainer'
+import { noResultQAData } from '@/consts'
 
-export default function Home() {
+export default function Search() {
+  const searchParams = useSearchParams()
+  const q = searchParams.get('q') || ''
   const { data, isLoading, isError, isFetching, fetchNextPage, hasNextPage } = useInfiniteQuery({
-    queryKey: ['newPosts'],
-    queryFn: ({ pageParam = 0 }) => fetchSlicePosts(pageParam),
+    queryKey: ['searchPosts', q],
+    queryFn: ({ pageParam = 0 }) => fetchSliceSearchPosts(pageParam, q),
     refetchOnWindowFocus: false,
     staleTime: Infinity,
     getNextPageParam: (lastPage) =>
@@ -21,20 +26,17 @@ export default function Home() {
 
   const pagesData = data?.pages ?? []
 
-  if (isError) {
-    return <div>エラーが発生しました</div>
-  }
-
   return (
     <main className={topPage}>
       <div className={formContainer}>
         <PostForm mode={'question'} />
       </div>
       <div className={mainContainer}>
-        <h2 className={qAListTitle}>最新の質問</h2>
-        {isError ? (
-          <div>エラーが発生しました</div>
-        ) : (
+        <h2 className={qAListTitle}>
+          「{q}」の検索結果：{pagesData[0]?.totalCount >= 0 ? pagesData[0]?.totalCount : '　'}件
+        </h2>
+        {isError && <div>エラーが発生しました</div>}
+        {isError || isLoading || pagesData[0]?.totalCount ? (
           <QAListWrapper
             pagesData={pagesData}
             isLoading={isLoading}
@@ -42,6 +44,10 @@ export default function Home() {
             fetchNextPage={fetchNextPage}
             hasNextPage={hasNextPage ?? false}
           />
+        ) : (
+          <div className={noResult}>
+            <QACardContainer qAData={noResultQAData} />
+          </div>
         )}
       </div>
     </main>
