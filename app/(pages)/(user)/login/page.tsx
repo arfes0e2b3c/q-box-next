31dc151491f54c0f5772f510612f4cfa28022ec2
arfes0e2b3c <webyata@gmail.com>
2023-11-11 17:21:40 +0900

    feat: ログイン機能の切り出し

--- app/(pages)/(user)/login/page.tsx
+++ app/(pages)/(user)/login/page.tsx
@@ -1,23 +1,20 @@
 'use client'
 import { baseFont } from '@/consts/fonts'
 import { form, formInner, formInput, formTitle, loginContainer, submitButton } from './page.css'
-import { auth } from '@/lib/firebase'
-import { signInWithEmailAndPassword } from 'firebase/auth'
 import { useRouter } from 'next/navigation'
 import { useForm } from 'react-hook-form'
-import { useCookies } from 'react-cookie'
+import { useFirebaseLogin } from '@/app/client/useFirebaseLogin'
 
 export default function Login() {
   const router = useRouter()
   const { register, handleSubmit } = useForm()
-  const [_, setCookie] = useCookies()
+  const firebaseLogin = useFirebaseLogin()
 
   const onSubmit = handleSubmit(async (event) => {
     const { email, password } = event
     try {
-      const res = await signInWithEmailAndPassword(auth, email, password)
-      setCookie('access_token', await res.user.getIdToken())
-      await router.push('/answer')
+      await firebaseLogin(email, password)
+      router.push('/answer')
     } catch (error) {
       await alert('ログイン時にエラーが発生しました\n' + error)
     }
