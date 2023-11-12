dffeb9f4569ca0bcaa1e3eafca58839836ca81da
arfes0e2b3c <webyata@gmail.com>
2023-11-12 13:56:16 +0900

    feat: 回答待ち質問ページを追加

---
+++ app/(pages)/(admin)/answer/page.css.ts
@@ -0,0 +1,120 @@
+import { style } from '@vanilla-extract/css'
+
+export const page = style({
+  width: '80%',
+  margin: '0 auto',
+  '@media': {
+    'screen and (max-width: 768px)': {
+      width: '100%',
+    },
+  },
+})
+
+export const pageInner = style({
+  display: 'flex',
+  flexDirection: 'column',
+  padding: '0',
+  margin: '20px 0',
+  '@media': {
+    'screen and (max-width: 768px)': {
+      width: '100%',
+    },
+  },
+})
+
+export const title = style({
+  fontSize: '2.2rem',
+  '@media': {
+    'screen and (max-width: 768px)': {
+      fontSize: '1.6rem',
+      marginLeft: '10px',
+    },
+  },
+})
+
+export const card = style({
+  display: 'flex',
+  alignItems: 'center',
+  flexDirection: 'column',
+  listStyle: 'none',
+  padding: '20px',
+  margin: '10px 0',
+  boxShadow: '0 0 5px 5px rgba(0, 0, 0, 0.1)',
+  overflowWrap: 'break-word',
+  '@media': {
+    'screen and (max-width: 768px)': {
+      padding: '10px',
+      margin: '10px 0',
+      boxShadow: '0 0 3px 3px rgba(0, 0, 0, 0.1)',
+    },
+  },
+})
+
+export const createdAt = style({
+  width: '120px',
+  padding: '3px 10px',
+  margin: '0',
+  borderRadius: '5px',
+  color: 'white',
+  backgroundColor: '#333',
+  border: '2px solid rgba(0, 0, 0, 1)',
+  whiteSpace: 'nowrap',
+  fontSize: '1rem',
+  textAlign: 'center',
+  '@media': {
+    'screen and (max-width: 768px)': {
+      width: '100px',
+      fontSize: '0.8rem',
+    },
+  },
+})
+
+export const box = style({
+  width: '100%',
+  display: 'flex',
+  justifyContent: 'space-between',
+  alignItems: 'center',
+})
+
+export const button = style({
+  width: '100px',
+  height: '30px',
+  border: '1px solid rgba(0, 0, 0, 0.3)',
+  borderRadius: '5px',
+  background: 'none',
+  transition: '0.5s',
+  cursor: 'pointer',
+  ':hover': {
+    backgroundColor: '#d77',
+    border: '1px solid rgb(200, 0, 0)',
+    color: 'white',
+  },
+  '@media': {
+    'screen and (max-width: 768px)': {
+      width: '50px',
+      height: '70px',
+      margin: '5px 0',
+    },
+  },
+})
+
+export const question = style({
+  width: '70%',
+  textAlign: 'center',
+  whiteSpace: 'pre-wrap',
+  fontSize: '1rem',
+  '@media': {
+    'screen and (max-width: 768px)': {
+      width: 'auto',
+      fontSize: '1rem',
+      maxWidth: 'calc(100% - 120px)',
+    },
+  },
+})
+
+export const toggleButton = style({
+  ':hover': {
+    backgroundColor: 'rgb(48,48,48)',
+    borderColor: 'rgb(48,48,48)',
+  },
+})
---
+++ app/(pages)/(admin)/answer/page.tsx
@@ -0,0 +1,44 @@
+import { fetchSlicePosts } from '@/app/client/fetchSlicePosts'
+import dayjs from 'dayjs'
+import {
+  box,
+  button,
+  card,
+  createdAt,
+  page,
+  pageInner,
+  question,
+  title,
+  toggleButton,
+} from './page.css'
+import { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: '管理者ページ',
+  description: 'お手伝いサークルの管理者用ページです。',
+}
+
+export default async function Answer() {
+  const posts = await fetchSlicePosts(0)
+  return (
+    <main className={page}>
+      <ul className={pageInner}>
+        <div>
+          <h2 className={title}>回答待ちの質問：未回答{posts.totalCount}件</h2>
+        </div>
+        {/* <p v-show='!posts[0]'>質問はありません</p> */}
+        {posts.contents.map((post) => (
+          <li className={card} key={post.id}>
+            <p className={createdAt}>{dayjs(post.createdAt).format('MM/DD HH:mm')}</p>
+            <div className={box}>
+              <button className={button}>削除</button>
+              <h3 className={question}>{post.question}</h3>
+              <button className={[button, toggleButton].join(' ')}>開閉</button>
+            </div>
+            {/* <SharedAnswerSendSentence className='send-sentence' /> */}
+          </li>
+        ))}
+      </ul>
+    </main>
+  )
+}
