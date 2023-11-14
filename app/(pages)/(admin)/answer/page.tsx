37eaa782033c7c5a48095a600c4210df92b9af0b
arfes0e2b3c <webyata@gmail.com>
2023-11-14 13:27:56 +0900

    refactor: コンポーネントの切り出し

--- app/(pages)/(admin)/answer/page.tsx
+++ app/(pages)/(admin)/answer/page.tsx
@@ -1,18 +1,7 @@
 import { fetchSlicePosts } from '@/app/client/fetchSlicePosts'
-import dayjs from 'dayjs'
-import {
-  box,
-  button,
-  card,
-  createdAt,
-  page,
-  pageInner,
-  question,
-  title,
-  toggleButton,
-} from './page.css'
+import { page, pageInner, title } from './page.css'
 import { Metadata } from 'next'
-import { baseFont } from '@/consts/fonts'
+import { AnswerCardForAnswer } from '@/components/shared/AnswerCardForAnswer'
 
 export const metadata: Metadata = {
   title: '管理者ページ',
@@ -21,6 +10,7 @@ export const metadata: Metadata = {
 
 export default async function Answer() {
   const posts = await fetchSlicePosts(0)
+
   return (
     <main className={page}>
       <h2 className={title}>回答待ちの質問：未回答{posts.totalCount}件</h2>
@@ -28,17 +18,7 @@ export default async function Answer() {
         {posts.contents.length ? (
           <>
             {posts.contents.map((post) => (
-              <li className={card} key={post.id}>
-                <p className={createdAt}>{dayjs(post.createdAt).format('MM/DD HH:mm')}</p>
-                <div className={box}>
-                  <button className={[button, baseFont.className].join(' ')}>削除</button>
-                  <h3 className={question}>{post.question}</h3>
-                  <button className={[button, toggleButton, baseFont.className].join(' ')}>
-                    開閉
-                  </button>
-                </div>
-                {/* <SharedAnswerSendSentence className='send-sentence' /> */}
-              </li>
+              <AnswerCardForAnswer key={post.id} post={post} />
             ))}
           </>
         ) : (
