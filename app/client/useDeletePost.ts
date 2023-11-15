e2835670a0e8a8dc45c17b5b1f261f8d3e819d4a
arfes0e2b3c <webyata@gmail.com>
2023-11-15 13:32:02 +0900

    refactor: 不要な括弧の削除

--- app/client/useDeletePost.ts
+++ app/client/useDeletePost.ts
@@ -1,9 +1,7 @@
 import { useMutation } from '@tanstack/react-query'
 
 export const useDeletePost = () =>
-  useMutation(async (contentId: string) => {
-    await deletePost(contentId)
-  })
+  useMutation(async (contentId: string) => await deletePost(contentId))
 
 const deletePost = async (contentId: string) => {
   const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/posts`, {
