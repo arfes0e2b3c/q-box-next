587b52f335e515aa7cfdd371700ccba35be9022d
arfes0e2b3c <webyata@gmail.com>
2023-11-17 16:50:53 +0900

    feat: 返信先ツイートIDの保存機能を追加

---
+++ app/api/posts/tweet_id/[contentId]/route.ts
@@ -0,0 +1,10 @@
+import { NextRequest, NextResponse } from 'next/server'
+import { patchPost } from '../../../microcms'
+
+export async function PATCH(req: NextRequest, { params }: { params: { contentId: string } }) {
+  const props = await req.json()
+  if (props.question === '')
+    return NextResponse.json({ error: 'ツイートのIDが存在しません' }, { status: 400 })
+  const res = await patchPost(params.contentId, { replyTweetId: props.tweetId })
+  return NextResponse.json(res, { status: 200 })
+}
--- app/client/usePostAnswer.ts
+++ app/client/usePostAnswer.ts
@@ -6,6 +6,7 @@ export const usePostAnswer = () =>
     const patchAnswerRes = await patchAnswer(answer, contentId)
     if (patchAnswerRes.error) throw new Error(patchAnswerRes.error)
     const tweetId = await postTweetThread(answer)
+    await patchTweetId(contentId, tweetId)
   })
 
 const patchAnswer = async (answer: string, contentId: string) => {
@@ -64,4 +65,16 @@ const postTweetReplies = async (firstReplyId: string, replies: string[]) => {
   const data: { id: string; error: string } = await res.json()
   if (res.status !== 200) throw new Error(data.error)
   return data.id
+}
+
+const patchTweetId = async (contentId: string, tweetId: string) => {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/posts/tweet_id/${contentId}`, {
+    method: 'PATCH',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ tweetId }),
+  })
+  const data: { id: string; error: string } = await res.json()
+  if (res.status !== 200) throw new Error(data.error)
 }
