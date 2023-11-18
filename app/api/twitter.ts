88351e2194f63b63762e28fca1ba48bbdef62029
arfes0e2b3c <webyata@gmail.com>
2023-11-18 17:54:09 +0900

    refactor: 変数名の修正

--- app/api/twitter.ts
+++ app/api/twitter.ts
@@ -12,10 +12,10 @@ export const postTweet = async (text: string) => {
   return res.data.id
 }
 
-export const postReply = async (text: string, ReplyTweetId: string): Promise<string> => {
+export const postReply = async (text: string, replyTweetId: string): Promise<string> => {
   const res = await twitterClient.v2.tweet(text, {
     reply: {
-      in_reply_to_tweet_id: ReplyTweetId,
+      in_reply_to_tweet_id: replyTweetId,
     },
   })
   return res.data.id
