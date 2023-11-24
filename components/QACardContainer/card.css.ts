133244a50e7590b96b415c342a138493bc808abc
arfes0e2b3c <webyata@gmail.com>
2023-11-24 18:30:26 +0900

    feat: 画像の出現アニメーションを追加

--- components/QACardContainer/card.css.ts
+++ components/QACardContainer/card.css.ts
@@ -1,8 +1,15 @@
-import { style } from '@vanilla-extract/css'
+import { keyframes, style } from '@vanilla-extract/css'
+
+const animate = keyframes({
+  '0%': { opacity: '0' },
+  '100%': { opacity: '1' },
+})
 
 export const qACard = style({
   width: '100%',
   height: 'auto',
   aspectRatio: '40 / 21',
   margin: '0 auto',
+  animation: `${animate} 0.2s ease-out forwards`,
+  opacity: '0',
 })
