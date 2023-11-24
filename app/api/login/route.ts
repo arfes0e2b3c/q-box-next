a9d407da343fab6a99d3619f394fa30dd6cb186c
arfes0e2b3c <webyata@gmail.com>
2023-11-24 16:20:18 +0900

    refactor: ログイン処理のリファクタ

--- app/api/login/route.ts
+++ app/api/login/route.ts
@@ -1,13 +1,13 @@
 import { NextRequest, NextResponse } from 'next/server'
 import { isRightAccessUser, loginFirebase } from '../firebase'
 
-export async function POST(req: NextRequest) {
-  const props = await req.json()
-  if (props.email === '')
+export async function POST(req: NextRequest): Promise<NextResponse> {
+  const { email, password } = await req.json()
+  if (!email)
     return NextResponse.json({ error: 'メールアドレスを入力してください' }, { status: 400 })
-  if (props.password === '')
+  if (!password)
     return NextResponse.json({ error: 'パスワードを入力してください' }, { status: 400 })
-  const res = await loginFirebase(props.email, props.password)
+  const res = await loginFirebase(email, password)
   return NextResponse.json(res, { status: 200 })
 }
 
@@ -16,6 +16,7 @@ export async function GET(req: NextRequest): Promise<NextResponse<boolean>> {
   const email = searchParams.get('email')
   const uid = searchParams.get('uid')
   if (!email || !uid) return NextResponse.json(false, { status: 400 })
-  if (!isRightAccessUser(email, uid)) return NextResponse.json(false, { status: 400 })
-  else return NextResponse.json(true, { status: 200 })
+  return isRightAccessUser(email, uid)
+    ? NextResponse.json(true, { status: 200 })
+    : NextResponse.json(false, { status: 400 })
 }
