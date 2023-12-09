624f341535a7291281370ab6f0d3d112b09fe7a8
arfes0e2b3c <webyata@gmail.com>
2023-12-09 17:25:56 +0900

    refactor: animationを一部修正

--- components/shared/MotionWrapper/index.tsx
+++ components/shared/MotionWrapper/index.tsx
@@ -8,9 +8,10 @@ export default function MotionWrapper({ children }: { children: React.ReactNode 
     <AnimatePresence mode='wait'>
       <motion.div
         key={pathName}
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        exit={{ opacity: 0 }}
+        initial={{ opacity: 0, y: 5 }}
+        animate={{ opacity: 1, y: 0 }}
+        exit={{ opacity: 0, y: 5 }}
+        transition={{ duration: 0.3, ease: 'easeOut' }}
       >
         {children}
       </motion.div>
