a85fb896297ffe8ed91135482763efc61fcf19a1
arfes0e2b3c <webyata@gmail.com>
2023-12-09 15:07:19 +0900

    feat: 404ページの追加

--- consts/index.ts
+++ consts/index.ts
@@ -16,6 +16,16 @@ export const noResultQAData: QA = {
   replies: [],
 }
 
+export const notFoundQAData: QA = {
+  id: 'noResult.png',
+  createdAt: '1111-11-10T15:00:00.000Z',
+  question: '',
+  answer: '',
+  replyTweetId: '',
+  state: 'noResult',
+  replies: [],
+}
+
 export const twitterMaxLength = 280
 export const tweetBaseText = `\n#お手伝いサークル ${appBaseUrl}`
 export const replyBaseText = `【提供していただいた情報】\n\n`
