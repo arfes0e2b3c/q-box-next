277800217ec039f7d5d1a774fe9bd41533f050ae
arfes0e2b3c <webyata@gmail.com>
2024-01-03 15:39:02 +0900

    refactor: 軽微な修正

--- lib/index.ts
+++ lib/index.ts
@@ -1,7 +1,6 @@
 import { answered, requirement, old, noResult } from '@/components/QACardContainer/index.css'
-import { continueText, twitterMaxLength } from '@/consts'
+import { continueText, imgFontSettings, twitterMaxLength } from '@/consts'
 import { AnswerState, MicroCMSResponse, QA } from '@/types'
-import path from 'path'
 import twitterText from 'twitter-text'
 
 export const filterPublicReplies = (post: QA): QA => {
@@ -40,7 +39,6 @@ export const exchangeStateToUrl = (state: AnswerState): string => {
     default:
       return baseUrl + '/690434409f8a4b2f9e53fe9f8dd23102/answered-right.png'
   }
-  return baseUrl + path
 }
 
 export const filterPostsHasOpenReply = (data: MicroCMSResponse): MicroCMSResponse => {
