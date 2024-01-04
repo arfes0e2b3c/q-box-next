8905f4e3aeacf203997dd6c6074a4958bcdab8ae
arfes0e2b3c <webyata@gmail.com>
2024-01-04 15:46:44 +0900

    feat: @vercel/analyticsの追加

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,14 @@
 import 'sanitize.css'
+import { Analytics } from '@vercel/analytics/react'
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang='ja'>
-      <body>{children}</body>
+      <body>
+        <p>わろた</p>
+        {children}
+        <Analytics />
+      </body>
     </html>
   )
 }
