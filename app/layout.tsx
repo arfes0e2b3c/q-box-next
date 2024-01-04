c04c11ce9e6099b08da1613877896e002411ccd7
arfes0e2b3c <webyata@gmail.com>
2024-01-04 15:47:11 +0900

    fix: 不要なタグの削除

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,6 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang='ja'>
       <body>
-        <p>わろた</p>
         {children}
         <Analytics />
       </body>
