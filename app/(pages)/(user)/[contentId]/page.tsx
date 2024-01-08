736df810715d0c6551f39d8bbe07c3bdd9c24b89
arfes0e2b3c <webyata@gmail.com>
2024-01-08 21:11:09 +0900

    fix: ogpのtitleをスペースに変更

--- app/(pages)/(user)/[contentId]/page.tsx
+++ app/(pages)/(user)/[contentId]/page.tsx
@@ -42,6 +42,7 @@ export const generateMetadata = async ({ params }: { params: { contentId: string
     },
     twitter: {
       card: 'summary_large_image',
+      title: ' ',
       description: 'お手伝いサークル公式サイト',
       images: [
         {
